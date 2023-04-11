9d821a13e397e6d7ffba2ae3bb713a2b8ef9ec84
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-11 19:44:18 +0300

    [client.cpp] cmd_opts(), return codes, exception handling, docs

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -7,6 +7,7 @@
 
 #include <algorithm>            // std::copy
 #include <any>                  // std::any_cast()
+#include <exception>
 #include <iostream>
 #include <iterator>             // std::ostream_iterator
 #include <string>               // std::to_string
@@ -14,6 +15,11 @@
 #include <utility>              // std::forward
 #include <vector>
 
+// catch all possible exceptions (like Pokemons)
+// disabled by default -> to not suppress core dumps etc.
+#ifndef CATCH_THEM_ALL
+#define CATCH_THEM_ALL 0
+#endif // !CATCH_THEM_ALL
 
 /*
 inline constexpr std::string
@@ -37,44 +43,85 @@ inline constexpr std::string
 dvw(std::string_view const& value) noexcept
 {
   return static_cast<std::string>(value);
+  // XXX: which is better?
+  // return std::string{ value };
 }
 
-int main (int argc, char *argv[])
+/**
+ * @brief parse command line options.
+ *
+ * catches every possible exception & signifies about that:
+ * with an error message printed to std::cerr.
+ * and with the return code / exit code.
+ *
+ * @param  argc - as in the usual main().
+ * @param  argv - as in the usual main().
+ * @return error code.
+ */
+int cmd_opts(int argc, const char *argv[])
 {
   using namespace std::literals; // XXX
-
   using namespace mqlqd;
 
-  // output every passed command line argument on stdout on separate lines.
-  // std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n")); // XXX
+  try
+  {
+    cxxopts::Options options("mqlqd_client",
+        "Transfer file(s) over TCP/IP to the server running the mqlqd_daemon.");
+    options.add_options()
+      ("a,addr", "Server IP address with the mqlqd_daemon.",
+       cxxopts::value<cmd_opt_t>()->default_value(dvw(cfg::def_addr)))
 
-  cxxopts::Options options("mqlqd_client",
-      "Transfer file(s) over TCP/IP to the server running the mqlqd_daemon.");
-  options.add_options()
-    ("a,addr", "Server IP address with the mqlqd_daemon.",
-     cxxopts::value<cmd_opt_t>()->default_value(dvw(cfg::def_addr)))
+      ("p,port", "Port number of the daemon on the server.",
+       cxxopts::value<cmd_opt_t>()->default_value(dvw(cfg::def_port)))
 
-    ("p,port", "Port number of the daemon on the server.",
-     cxxopts::value<cmd_opt_t>()->default_value(dvw(cfg::def_port)))
+      ("f,file", "File path of the file to transmit.", cxxopts::value<cmd_opt_t>())
+      ("h,help", "Show usage help.")
+      ("file_paths", "File path(s) as trailing argument(s).",
+       cxxopts::value<std::vector<cmd_opt_t>>());
+    // to support multiple files as trailing arguments: file1.txt file2.txt file3.txt
+    // XXX: UNIMPLEMENTED
+    options.parse_positional({"file_paths"});
+    auto opts{ options.parse(argc, argv) };
 
-    ("f,file", "File path of the file to transmit.", cxxopts::value<cmd_opt_t>())
-    ("h,help", "Show usage help.")
-    ("file_paths", "File path(s) as trailing argument(s).",
-     cxxopts::value<std::vector<cmd_opt_t>>());
-  // to support multiple files as trailing arguments: file1.txt file2.txt file3.txt
-  // XXX: UNIMPLEMENTED
-  options.parse_positional({"file_paths"});
-  auto opts{ options.parse(argc, argv) };
+    if (opts.count("help")) {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
 
-  if (opts.count("help")) {
-    std::cout << options.help() << std::endl;
-    exit(0);
+    if (opts.count("file")) {
+      try {
+        std::cout << opts["file"].as<cmd_opt_t>() << '\n';
+      } catch(std::bad_cast const& err) {
+        std::cerr << "ERROR: cxxopts opts as miscast - during parsing of the cmd options:" << '\n'
+          << err.what() << '\n';
+        throw; // rethrow
+      }
+    }
+
+  } catch(cxxopts::exceptions::exception const& err) {
+    std::cerr << "ERROR: during parsing of the cmd options:" << '\n'
+      << err.what() << '\n';
+    return 1;
+#if CATCH_THEM_ALL
+  } catch(std::exception const& err) {
+    std::cerr << "CRITICAL ERROR: an unhandled std::exception was caught:" << '\n'
+      << err.what() << '\n';
+    return 2;
+  } catch(...) {
+    std::cerr << "CRITICAL ERROR: an unhandled anonymous exception occurred!" << '\n'
+      << "THIS IS VERY BAD!" << '\n';
+    return 3;
+#endif // CATCH_THEM_ALL
   }
+  return 0;
+}
 
-  if (opts.count("file")) {
-    // std::cout << opts["file"].as<cmd_opt_t>() << '\n';
-    std::cout << opts["file"].as<sv_t>() << '\n';
-  }
+int main(int argc, const char *argv[])
+{
+  int return_code{ -1 }; // also known as the error code
+  return_code = cmd_opts(argc, argv);
+  if (return_code != 0) return return_code;
+  // potential place for the extra steps (for the future build up)
 
   return 0;
 }
