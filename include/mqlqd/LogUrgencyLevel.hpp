9c50bb059db965947220791c4ab26df19758e223
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-15 11:45:45 +0300

    [LogUrgencyLevel.hpp] fmt format enum log levels in the readable text form.

--- include/mqlqd/LogUrgencyLevel.hpp
+++ include/mqlqd/LogUrgencyLevel.hpp
@@ -1,6 +1,9 @@
 #pragma once
 // Logger message urgency levels.
 
+#include <fmt/format.h>
+
+
 namespace mqlqd {
 
 // log msg can have different log levels.
@@ -17,4 +20,27 @@ enum class LogUrgencyLevel
 };
 
 } // namespace mqlqd
+
+// NOTE: fmt demands to specialize/declare this here!
+// [template_spec_redecl_out_of_scope].
+// @brief format enum log levels in the readable text form.
+template <> struct
+fmt::formatter<mqlqd::LogUrgencyLevel> : formatter<string_view> {
+  // parse is inherited from formatter<string_view>.
+  using LL = mqlqd::LogUrgencyLevel;
+  template <typename FormatContext>
+  auto format(LL el, FormatContext& ctx) const {
+    string_view name = "unknown";
+    switch (el) {
+    case LL::DBUG: name = "DBUG"; break;
+    case LL::INFO: name = "INFO"; break;
+    case LL::STAT: name = "STAT"; break;
+    case LL::NTFY: name = "NTFY"; break;
+    case LL::WARN: name = "WARN"; break;
+    case LL::ERRO: name = "ERRO"; break;
+    case LL::CRIT: name = "CRIT"; break;
+    }
+    return formatter<string_view>::format(name, ctx);
+  }
+};
 
