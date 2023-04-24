e51b63c8326a0e4c97d20e38bedc76c5d5dfecb0
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-24 13:31:21 +0300

    [config.hpp] no need for the string analogs of the variables anymore

--- include/mqlqd/config.hpp
+++ include/mqlqd/config.hpp
@@ -5,29 +5,28 @@
 namespace mqlqd {
 namespace cfg {
 
-inline constexpr addr_t addr{ "127.0.0.1"sv };
-inline constexpr port_t port{ 42069 }; // u16 max!
-
+inline constexpr addr_t addr{ "127.0.0.1" };  // i.e. localhost
+inline constexpr port_t port{ 42069 };        // u16 max!
 
 // log urgency level
 // inline constexpr LL urgency{ LL::NTFY }; // default for the basic users
 inline constexpr LL urgency{ LL::DBUG }; // development level (all messages)
 
 // default log file path
-inline constexpr sv_t log_fpath{ "/tmp/mqlqd/logs/default.log"sv };
+inline constexpr sv_t log_fpath{ "/tmp/mqlqd/logs/default.log" };
+
+// default (client UID / device MAC) unique to the client/device.
+inline constexpr sv_t def_uid{ "f000::f000:f000:f000:f000" };
 
 /**
  * NOTE: Keep following variables up to date with the above!
  * => following variables are only for the representation
  * in the simple text form of the default config values!
  * NOTE: cxxopts default_value() accepts std::string etc.
+ *
+ * XXX : I think there is no need in duplicates anymore.
+ *      let this note live here for a while.
  */
-
-inline constexpr sv_t def_addr{ "127.0.0.1"sv }; // i.e. localhost
-inline constexpr sv_t def_port{ "69042"sv };
-
-// default (client UID / device MAC) unique to the client/device.
-inline constexpr sv_t def_uid{ "f000::f000:f000:f000:f000"sv };
 
 } // namespace cfg
 } // namespace mqlqd
