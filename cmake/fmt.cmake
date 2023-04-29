8b992799a4963ab2055f908acf730607258e3d5a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-29 14:06:01 +0300

    [intermediate] fix: fmt linking 2

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,6 +1,9 @@
 ## prefer fmtlib version installed in the system || fetch if not found
-find_package(fmt 9.1.0)
-if(NOT fmt_FOUND)
+
+
+# find_package(fmt 9.1.0)
+# if(NOT fmt_FOUND)
+if(TRUE)
   message(">> fetching fmt of required version!")
   include(FetchContent)
   FetchContent_Declare(
@@ -11,4 +14,13 @@ if(NOT fmt_FOUND)
 else()
   message(">> found fmt of required version!")
 endif()
+
+get_filename_component(fmt_src
+  ${PROJECT_BINARY_DIR}/_deps/fmt-src REALPATH
+)
+# target_include_directories(mqlqd_deps PUBLIC ${fmt_src}/include)
+target_include_directories(mqlqd_deps PRIVATE ${fmt_src}/include)
+# target_precompile_headers(mqlqd_deps PRIVATE ${fmt_src}/cxxopts.hpp)
+
+# add_subdirectory(${fmt_src})
 
