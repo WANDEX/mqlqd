3b69737bccee35957a163b1647c1781352db3a56
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-04-30 17:27:13 +0300

    [fmt.cmake] upd

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -3,9 +3,9 @@
 cmake_path(APPEND dep_dir ${PROJECT_BINARY_DIR} "_deps")
 
 cmake_path(APPEND fmt_dir ${dep_dir} "fmt") # lib dir root
+cmake_path(APPEND fmt_sub ${fmt_dir} "sub")
 cmake_path(APPEND fmt_src ${fmt_dir} "src")
-cmake_path(APPEND fmt_bld ${fmt_dir} "build")
-cmake_path(APPEND fmt_sub ${fmt_dir} "subbuild")
+cmake_path(APPEND fmt_bin ${fmt_dir} "bin")
 
 # find_package(fmt 9.1.0)
 # if(NOT fmt_FOUND)
@@ -15,29 +15,34 @@ if(TRUE)
   FetchContent_Declare(fmt
     GIT_REPOSITORY    https://github.com/fmtlib/fmt.git
     GIT_TAG           9.1.0
+    SUBBUILD_DIR      ${fmt_sub}
     SOURCE_DIR        ${fmt_src}
-    SUBBUILD_DIR      ${fmt_bld}
-    BINARY_DIR        ${fmt_sub}
+    BINARY_DIR        ${fmt_bin}
   )
+  option(FMT_INSTALL "" ON)
   FetchContent_MakeAvailable(fmt)
 else()
   message(">> found fmt of required version!")
 endif()
 
-# get_filename_component(fmt_src ${PROJECT_BINARY_DIR}/_deps/fmt-src REALPATH)
+target_include_directories(mqlqd_deps PUBLIC ${fmt_src}/include)
 
-# target_include_directories(mqlqd_deps PUBLIC ${fmt_src}/include)
-# target_include_directories(mqlqd_deps PRIVATE ${fmt_src}/include)
-# target_precompile_headers(mqlqd_deps PRIVATE ${fmt_src}/cxxopts.hpp)
+# target_link_libraries(mqlqd_deps libfmtd.a)
+# target_link_libraries(mqlqd_deps -L${fmt_bin})
 
-# target_include_directories(mqlqd_deps INTERFACE ${fmt_src}/include)
-# target_include_directories(mqlqd_deps PUBLIC ${fmt_src}/include)
-target_include_directories(mqlqd_deps PRIVATE ${fmt_src}/include)
+# target_link_libraries(mqlqd_deps -lfmt)
 
-# add_subdirectory(${fmt_src})
+# target_link_libraries(mqlqd_deps -I${fmt_src}/include)
 
-target_link_libraries(mqlqd_deps libfmtd.a)
-target_link_libraries(mqlqd_deps -L${fmt_sub})
+
+find_library(FMT_LIB fmt)
+# This call requires CMake 3.13 or later
+target_link_libraries(mqlqd_deps PUBLIC ${FMT_LIB})
+
 
 # target_link_libraries(mqlqd_deps PRIVATE fmt::fmt)
+# target_link_libraries(mqlqd_deps PUBLIC fmt::fmt)
+
+# target_link_libraries(mqlqd_depsi INTERFACE fmt::fmt-header-only)
+# target_link_libraries(mqlqd_deps PUBLIC fmt::fmt-header-only)
 
