613eafc734c5b55b0cf2c3d0164d4e07bd18ec12
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-05-01 23:26:00 +0300

    Revert "[cxxopts.cmake] upd"
    This reverts commit 02166edc7e1a46a638baa800ee193f1a20da4797.

--- cmake/cxxopts.cmake
+++ cmake/cxxopts.cmake
@@ -1,24 +1,13 @@
 ## cxxopts fetch
 ## for the command line options.
 
-# find_package(cxxopts 3.1.1)
-# if(NOT cxxopts_FOUND)
-if(TRUE) # XXX: ^ will not work cxxopts has weird configuration...
-  # message(">> fetching cxxopts of required version!")
-  include(FetchContent)
-  FetchContent_Declare(
-    cxxopts
-    GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
-    GIT_TAG v3.1.1
-  )
-  FetchContent_MakeAvailable(cxxopts)
-else()
-  message(">> found cxxopts of required version!")
-endif()
-
-get_filename_component(cxxopts_incld
-  ${PROJECT_BINARY_DIR}/_deps/cxxopts-src/include REALPATH
+include(FetchContent)
+FetchContent_Declare(
+  cxxopts
+  GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
+  GIT_TAG v3.1.1
 )
-target_include_directories(mqlqd_deps PUBLIC ${cxxopts_incld})
-target_precompile_headers(mqlqd_deps PRIVATE ${cxxopts_incld}/cxxopts.hpp)
+FetchContent_MakeAvailable(cxxopts)
+
+add_subdirectory(${PROJECT_BINARY_DIR}/_deps/cxxopts-src/include)
 
