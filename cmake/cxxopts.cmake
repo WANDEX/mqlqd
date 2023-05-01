ec716c874f26d6ddeb8a28c5dbfc9e3d34e7aa3f
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-05-01 23:26:00 +0300

    Revert "[cxxopts.cmake] upd"
    This reverts commit 5b7e5466763e243c7f52663f0adf8f2a86f1eda7.

--- cmake/cxxopts.cmake
+++ cmake/cxxopts.cmake
@@ -1,33 +1,24 @@
 ## cxxopts fetch
 ## for the command line options.
 
-cmake_path(APPEND dep_dir ${PROJECT_BINARY_DIR} "_deps")
-
-cmake_path(APPEND cxxopts_dir ${dep_dir}     "cxxopts") # lib dir root
-cmake_path(APPEND cxxopts_sub ${cxxopts_dir} "sub")
-cmake_path(APPEND cxxopts_src ${cxxopts_dir} "src")
-cmake_path(APPEND cxxopts_bin ${cxxopts_dir} "bin")
-
 # find_package(cxxopts 3.1.1)
 # if(NOT cxxopts_FOUND)
 if(TRUE) # XXX: ^ will not work cxxopts has weird configuration...
-  message(">> fetching cxxopts of required version!")
+  # message(">> fetching cxxopts of required version!")
   include(FetchContent)
-  FetchContent_Declare(cxxopts
-    GIT_REPOSITORY    https://github.com/jarro2783/cxxopts.git
-    GIT_TAG           v3.1.1
-    SUBBUILD_DIR      ${cxxopts_sub}
-    SOURCE_DIR        ${cxxopts_src}
-    BINARY_DIR        ${cxxopts_bin}
+  FetchContent_Declare(
+    cxxopts
+    GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
+    GIT_TAG v3.1.1
   )
   FetchContent_MakeAvailable(cxxopts)
 else()
   message(">> found cxxopts of required version!")
 endif()
 
-# target_include_directories(mqlqd_deps PUBLIC ${cxxopts_src}/include)
-# target_precompile_headers(mqlqd_deps PRIVATE ${cxxopts_src}/include/cxxopts.hpp)
-
-target_include_directories(mqlqd_bin_i INTERFACE ${cxxopts_src}/include)
-target_precompile_headers (mqlqd_bin_i INTERFACE ${cxxopts_src}/include/cxxopts.hpp)
+get_filename_component(cxxopts_incld
+  ${PROJECT_BINARY_DIR}/_deps/cxxopts-src/include REALPATH
+)
+target_include_directories(mqlqd_deps PUBLIC ${cxxopts_incld})
+target_precompile_headers(mqlqd_deps PRIVATE ${cxxopts_incld}/cxxopts.hpp)
 
