664a8376e25e12c83558e93d3eb154dfd9554d35
WANDEX <15724752+WANDEX@users.noreply.github.com>
2023-05-08 17:16:24 +0300

    [gtest.cmake] [intermediate] [dirty] 2

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -7,36 +7,71 @@ cmake_path(APPEND gtest_sub ${gtest_dir} "sub")
 cmake_path(APPEND gtest_src ${gtest_dir} "src")
 cmake_path(APPEND gtest_bin ${gtest_dir} "bin")
 
-# find_package(GTest 1.12.1)
+add_library(tests_deps "")
+# add_library(tests_deps SHARED)
 
-# find_package(PkgConfig)
-# if(NOT GTest_FOUND)
 include(FindPkgConfig)
-pkg_search_module(GTEST REQUIRED gtest_main>=1.13.0)
-if(${GTEST_MODULE_NAME} STREQUAL "")
-# if(TRUE)
+# pkg_search_module(GTEST gtest_main>=1.13.0)
+# pkg_search_module(GTEST gtest_main=1.12.1) # XXX
+# find_package(GTest 1.13.0)
+find_package(GTest 1.13.0)
+if(GTEST_FOUND)
+# if(GTest_FOUND)
+  message(">> found gtest.")
+  # find_package(GTest 1.13.0) # XXX
+else()
   message(">> fetch gtest.")
   include(FetchContent)
   FetchContent_Declare(googletest
     GIT_REPOSITORY    https://github.com/google/googletest.git
     GIT_TAG           v1.13.0
+    # GIT_TAG           release-1.12.1
     SUBBUILD_DIR      ${gtest_sub}
     SOURCE_DIR        ${gtest_src}
     BINARY_DIR        ${gtest_bin}
+    FIND_PACKAGE_ARGS NAMES GTest
+    # OVERRIDE_FIND_PACKAGE
   )
+  option(BUILD_GMOCK   "" OFF)
+  option(INSTALL_GTEST "" OFF)
   FetchContent_MakeAvailable(googletest)
-else()
-  message(">> found gtest.")
+
+
+  # set(PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=yes)
+  # set(PKG_CONFIG_ALLOW_SYSTEM_LIBS=yes)
+
+  # set(PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=no)
+  # set(PKG_CONFIG_ALLOW_SYSTEM_LIBS=no)
+
+
+  # target_include_directories(tests_deps PUBLIC "${gtest_src}/googlemock/include")
+  # target_include_directories(tests_deps PUBLIC "${gtest_src}/googletest/include")
+  # target_include_directories(tests_deps PUBLIC "${gtest_src}/googletest/include")
+
+  ## link with the static library libfmtd.a
+  ## which is found in the fetched locally lib dir.
+  # target_link_libraries(tests_deps PRIVATE -L"${gtest_bin}/googletest" -lgtest_main)
+  # target_link_libraries(tests_deps PUBLIC -L"${gtest_bin}/googletest" -lgtest_main)
+  # target_link_libraries(tests_deps -L"${gtest_bin}/googletest" -lgtest_main)
+  # target_link_libraries(tests_deps -L"${gtest_bin}/googletest" -lgtest -lgtest_main -lpthread)
+
+  # include(GoogleTest)
 endif()
 
 ## For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
 include(GoogleTest)
 enable_testing()
 
 # add_library(tests_deps INTERFACE)
 # add_library(tests_deps SHARED "")
-add_library(tests_deps "")
-target_link_libraries(tests_deps ${GTEST_LDFLAGS})
-target_compile_options(tests_deps PUBLIC ${GTEST_CFLAGS})
+
+# target_link_libraries(tests_deps ${GTEST_LDFLAGS})
+# target_compile_options(tests_deps PUBLIC ${GTEST_CFLAGS})
+
+# target_link_libraries(tests_deps PUBLIC ${GTEST_LDFLAGS})
+# target_compile_options(tests_deps PUBLIC ${GTEST_CFLAGS})
+
+target_link_libraries(tests_deps PUBLIC GTest::gtest_main)
 
