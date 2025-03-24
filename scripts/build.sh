60b666007c3671b7c723abf4f7aba3ab959b21f4
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-03-25 01:26:01 +0300

    [build.sh] at_path(), notify(), project_name -> BUILT notification

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,13 +19,36 @@
 ## or
 # ./scripts/build.sh gtest *wildcard*
 
+set -e
+
+at_path() { hash "$1" >/dev/null 2>&1 ;} # if $1 is found at $PATH -> return 0
+
+notify() {
+    at_path notify-send || return 0
+    stag="string:x-dunst-stack-tag"
+    bg="string:bgcolor:"
+    fg="string:fgcolor:"
+    arg="$2"
+    case "$1" in
+      *error*|*ERROR*)     urg="critical" ;;
+      *warning*|*WARNING*) urg="normal" ;;
+      *)
+        urg="low"
+        bg="string:bgcolor:#11FF44"
+        fg="string:fgcolor:#000000"
+        arg="$1"
+      ;;
+    esac
+    notify-send -u "$urg" -h "$stag:wndx_sane" -h "$stag:hi" -h "$bg" -h "$fg" "[$project_name]" "$arg"
+}
+
+project_name=$(basename "$(git rev-parse --show-toplevel)")
+
 bt="${BUILD_TYPE:-Debug}"
 compiler="${CC:-_}"
 # get compiler basename in case declared via full path
 cmbn=$(basename "$compiler")
 bdir="build/dev-$bt-$cmbn"
-
-set -e
 
 beg_epoch=$(date +%s) # for the first call of the time_spent_on_step
 
@@ -35,6 +58,13 @@ if [ ! -r ./CMakeLists.txt ]; then
 fi
 
 [ -n "$CTEST_OUTPUT_ON_FAILURE" ] || export CTEST_OUTPUT_ON_FAILURE=1
+
+verbose=""
+cmake_log_level=""
+if false; then
+  verbose="--verbose"
+  cmake_log_level="--log-level=DEBUG"
+fi
 
 opt="$1"
 test_filter="$2"
@@ -80,10 +110,13 @@ vsep() {
 }
 
 vsep "CONFIGURE" "${BLU}"
-cmake -S . -B "$bdir" -G Ninja -D CMAKE_BUILD_TYPE="${bt}" -D MQLQD_BUILD_TESTS="${tt}" -Wdev -Werror=dev ${fresh}
+cmake -S . -B "$bdir" -G Ninja -D CMAKE_BUILD_TYPE="${bt}" -D MQLQD_BUILD_TESTS="${tt}" \
+-Wdev -Werror=dev ${fresh} ${cmake_log_level}
 
 vsep "BUILD" "${CYN}"
-cmake --build "$bdir" --config ${bt} ${clean_first}
+cmake --build "$bdir" --config "${bt}" ${clean_first} ${verbose}
+
+notify "BUILT"
 
 gtest_binary="./$bdir/tests/units/tests_units"
 if [ "$tt" = ON ] && [ ! -x "$gtest_binary" ]; then
