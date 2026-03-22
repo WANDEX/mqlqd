214559c2aa850f5ca4e3522bda900666c2751e10
WANDEX <15724752+WANDEX@users.noreply.github.com>
2026-03-22 23:59:59 +0300

    [file.t.cpp] compare operators & tmp dir

--- tests/units/file.t.cpp
+++ tests/units/file.t.cpp
@@ -5,24 +5,71 @@
 #include <cstring> // memcpy
 #include <filesystem>
 #include <string>
+#include <system_error>
 
 
 namespace wndx::mqlqd {
+
+[[nodiscard]]
+auto make_tmp_dir() noexcept
+{
+  rc              rc{ rc::FAILURE };
+  std::error_code ec{};
+  fs::path        tmp_dir{ fs::temp_directory_path(ec) };
+  if (ec) {
+    WNDX_LOG(LL::ERRO, "[FAIL] fs::temp_directory_path(ec) -> v:{} m:{}\n",
+             ec.value(), ec.message());
+  }
+  tmp_dir /= "wndx";
+  rc = wndx::sane::file::mkdir(tmp_dir, fs::perms::all);
+  EXPECT_TRUE(rc == rc::SUCCESS);
+  tmp_dir /= "mqlqd";
+  rc = wndx::sane::file::mkdir(tmp_dir, fs::perms::all);
+  EXPECT_TRUE(rc == rc::SUCCESS);
+  return tmp_dir.string();
+}
+
+static std::string_view const g_tmp_dir{ make_tmp_dir() };
 
 class File_test : public ::testing::Test
 {
 public:
   /// \brief helper function to create File from the tests data files.
-  static file::File alloc_file(std::string const& file_name = "ascii_1.txt")
+  [[nodiscard]]
+  file::File alloc_file(std::string const& file_name = "ascii_1.txt",
+                        std::string const& data_dir  = "./data") const noexcept
   {
     fs::path dp;
     fs::path fp;
     EXPECT_NO_THROW({
-      dp = "./data";
+      dp = data_dir;
       fp = dp / file_name;
     });
     file::File File(fp, fs::file_size(fp));
     return File;
+  }
+
+  [[nodiscard]]
+  static fs::path get_tmp_dir()
+  {
+    return g_tmp_dir;
+  }
+
+  [[nodiscard]]
+  file::File tmp_file(std::string const& file_name) const noexcept
+  {
+    auto tmp_dir{ get_tmp_dir() };
+    auto File{ alloc_file(file_name, tmp_dir) };
+    return File;
+  }
+
+  /// \brief read file and check that memory is allocated.
+  static void check_null(file::File& file) noexcept
+  {
+    ASSERT_TRUE(file.read_to_block() == rc::SUCCESS);
+    ASSERT_TRUE(file.memory() != nullptr);
+    ASSERT_TRUE(file);
+    ASSERT_FALSE(!file);
   }
 };
 
@@ -54,6 +101,66 @@ TEST_F(File_test, ctor_finfo_path)
   finfo2.m_fname[len] = '\0';
   std::memcpy(&finfo2.m_fname, file_name, len);
   ASSERT_EQ(std::string(finfo.m_fname), std::string(finfo2.m_fname));
+}
+
+TEST_F(File_test, compare_nullptr)
+{
+  auto file1{ alloc_file() };
+  auto file2{ alloc_file() };
+  ASSERT_TRUE(file1.memory() == nullptr);
+  ASSERT_TRUE(file2.memory() == nullptr);
+  ASSERT_FALSE(file1);
+  ASSERT_FALSE(file2);
+  ASSERT_TRUE(!file1);
+  ASSERT_TRUE(!file2);
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);  // assume not eq when nullptr!
+  ASSERT_FALSE(file1 == file2); // due to nullptr!
+  ASSERT_FALSE(file1 <= file2);
+  ASSERT_FALSE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_eq)
+{
+  auto file1{ alloc_file() };
+  auto file2{ alloc_file() };
+  check_null(file1);
+  check_null(file2);
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_FALSE(file1 != file2);
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_TRUE(file1 <= file2);
+  ASSERT_TRUE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_lt)
+{
+  auto file1{ alloc_file("ascii_1.txt") };
+  auto file2{ alloc_file("ascii_2.txt") };
+  check_null(file1);
+  check_null(file2);
+  ASSERT_TRUE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);
+  ASSERT_FALSE(file1 == file2);
+  ASSERT_TRUE(file1 <= file2);
+  ASSERT_FALSE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_gt)
+{
+  auto file1{ alloc_file("ascii_3.txt") };
+  auto file2{ alloc_file("ascii_2.txt") };
+  check_null(file1);
+  check_null(file2);
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_TRUE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);
+  ASSERT_FALSE(file1 == file2);
+  ASSERT_FALSE(file1 <= file2);
+  ASSERT_TRUE(file1 >= file2);
 }
 
 } // namespace wndx::mqlqd
