d20bb2860e7d0adf85857c1e18d92bfebc2a21ed
WANDEX <15724752+WANDEX@users.noreply.github.com>
2026-03-29 17:01:39 +0300

    extracted to wndx::sane File & File tests

--- include/wndx/mqlqd/file.hpp
+++ include/wndx/mqlqd/file.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
-#include "aliases.hpp"        // IWYU pragma: keep
+#include "aliases.hpp" // IWYU pragma: keep
 
-#include "wndx/sane/file.hpp" // IWYU pragma: keep
-
-#include <cstring>            // std::memcmp()
+#include "wndx/sane/file.hpp"
 
 
 namespace wndx::mqlqd::file {
@@ -21,139 +19,24 @@ struct Finfo
   char        m_fname[fname_max_len]{ "mqlqd_default_file_name\0" };
 };
 
-class File final
+class File : public wndx::sane::file::File
 {
 public:
-  /// to read/write/store all files as binary data with the unified underlying
-  /// type across the project code base and across devices.
-  /// NOTE: tried other types, specifically:
-  /// u8 - unsigned char type => break the logic:
-  /// printing/writing binary file contents (send/recv maybe also, but not sure)
-  /// Currently I do not see any benefit in wasting time on making this
-  /// possible using u8. Probably some extra handling required, which only
-  /// overcomplicate code. (so why bother?)
-  using char_type = char;
-
-  // clang-format off
-  static constexpr auto openmode_r{
-    std::ios::in  | std::ios::binary | std::ios::ate
-  };
-  static constexpr auto openmode_w{
-    std::ios::out | std::ios::binary
-  };
-  // clang-format on
-
-  File()                       = default;
+  File()                       = delete;
   File(File&&)                 = default;
   File(File const&)            = default;
   File& operator=(File&&)      = delete;
   File& operator=(File const&) = delete;
-  ~File() noexcept;
+  ~File() noexcept             = default;
 
   /// \brief construct class instance from the file path & its size.
   explicit File(fs::path fpath, std::size_t sz) noexcept;
 
   /// \brief construct class instance from the file info structure.
-  explicit File(Finfo const& finfo, fs::path const& dpath) noexcept;
-
-  /// \brief cleanup facilities for the heap.
-  void clean_heap() noexcept;
+  explicit File(Finfo const& finfo, fs::path dpath) noexcept;
 
   /// \brief convert essentials of the instance into file info structure.
   [[nodiscard]] Finfo to_finfo() const noexcept;
-
-  /// \brief allocate required space in the heap for the file contents.
-  [[nodiscard]] rc alloc() noexcept;
-
-  /// \brief write file to the disk.
-  [[nodiscard]] rc write() const noexcept;
-
-  /// \brief read file contents into the memory block.
-  [[nodiscard]] rc read() const noexcept;
-
-  /// \brief alloc space & read file contents into the memory block.
-  [[nodiscard]] rc alloc_and_read() noexcept;
-
-  /// \brief copy file to the destination.
-  [[nodiscard]] rc copy_to(fs::path const& dst) noexcept;
-
-  /// \brief move file to the destination.
-  [[nodiscard]] rc move_to(fs::path const& dst) noexcept;
-
-  /// \brief print file contents (like a UNIX cat utility).
-  void print() const noexcept;
-
-  /// \brief path to the file.
-  [[nodiscard]] fs::path path() const noexcept { return m_fpath; }
-
-  /// \brief size of contiguous memory block required for the file.
-  [[nodiscard]] constexpr std::size_t size() const noexcept
-  {
-    return m_block_size;
-  }
-
-  /// \brief memory block.
-  [[nodiscard]] constexpr char_type* memory() const noexcept { return m_block; }
-
-  /// \brief uses compiler intrinsics for the efficient memory comparison.
-  ///
-  /// \see https://en.cppreference.com/w/cpp/string/byte/memcmp
-  [[nodiscard]] static int memcmp(char_type const* const lhs,
-                                  char_type const* const rhs, std::size_t count)
-  {
-    return std::memcmp(lhs, rhs, count); // not constexpr!
-  }
-
-  /// \brief uses compiler intrinsics for comparing files memory blocks.
-  [[nodiscard]] int memcmp(char_type const* const rhs) const noexcept
-  {
-    return memcmp(memory(), rhs, size());
-  }
-
-  /// \brief allow conversion of the memory block pointer to bool.
-  constexpr explicit operator bool() const noexcept
-  {
-    return m_block != nullptr;
-  }
-
-  constexpr bool operator!=(File const& rhs) const noexcept
-  {
-    return !m_block || !rhs.m_block || size() != rhs.size();
-  }
-
-  constexpr bool operator<(File const& rhs) const noexcept
-  {
-    return operator!=(rhs) && size() < rhs.size();
-  }
-
-  constexpr bool operator>(File const& rhs) const noexcept
-  {
-    return operator!=(rhs) && size() > rhs.size();
-  }
-
-  /// \brief equality of the files contents (compare memory blocks).
-  ///
-  /// memory comparison is the most expensive operation,
-  /// so we compare memory at the very end, after all other conditions.
-  constexpr bool operator==(File const& rhs) const noexcept
-  {
-    return !operator!=(rhs) && memcmp(rhs.memory()) == 0;
-  }
-
-  constexpr bool operator<=(File const& rhs) const noexcept
-  {
-    return operator<(rhs) || operator==(rhs);
-  }
-
-  constexpr bool operator>=(File const& rhs) const noexcept
-  {
-    return operator>(rhs) || operator==(rhs);
-  }
-
-private:
-  std::size_t m_block_size{ 0 };
-  fs::path    m_fpath{};
-  char_type*  m_block{ nullptr }; // memory block -> contiguous chunk of memory.
 };
 
 } // namespace wndx::mqlqd::file
--- src/wndx/mqlqd/common/file.cpp
+++ src/wndx/mqlqd/common/file.cpp
@@ -1,9 +1,5 @@
 
-#include "wndx/mqlqd/aliases.hpp" // IWYU pragma: keep
-
 #include "wndx/mqlqd/file.hpp"
-
-#include <fstream>
 
 
 namespace wndx::mqlqd::file {
@@ -11,208 +7,32 @@ namespace wndx::mqlqd::file {
 static constexpr std::string_view ctor{ "ctor - File instance" };
 
 File::File(fs::path fpath, size_t sz) noexcept
-    : m_block_size{ sz }
-    , m_fpath{ std::move(fpath) }
+    : wndx::sane::file::File(std::move(fpath), sz)
 {
   WNDX_LOG(LL::DBUG, "{} from file & size:\n\t{}\n", ctor, *this);
 }
 
-File::File(Finfo const& finfo, fs::path const& dpath) noexcept
-    : m_block_size{ finfo.m_block_size }
-    , m_fpath{ dpath / std::string(finfo.m_fname) } // concatenate path
+// NOLINTNEXTLINE(performance-unnecessary-value-param)
+File::File(Finfo const& finfo, fs::path dpath) noexcept
+    // NOLINTNEXTLINE(*-array-to-pointer-decay, hicpp-no-array-decay)
+    : wndx::sane::file::File({ dpath / std::string(finfo.m_fname) },
+                             finfo.m_block_size)
 {
   WNDX_LOG(LL::DBUG, "{} from Finfo & dir path:\n\t{}\n", ctor, *this);
 }
 
-void File::clean_heap() noexcept
-{
-  delete[] m_block;
-  m_block = nullptr;
-}
-
-File::~File() noexcept { clean_heap(); }
-
 [[nodiscard]] Finfo File::to_finfo() const noexcept
 {
   Finfo finfo{};
-  finfo.m_block_size = m_block_size;
+  finfo.m_block_size = size();
   // fill the array - file name (element by element)
-  auto fname{ m_fpath.filename().string() };
+  auto fname{ path().filename().string() };
   for (size_t i = 0; i < fname.length(); ++i) {
     finfo.m_fname[i] = fname[i]; // NOLINT(*-constant-array-index)
   }
   // null terminator after the file name (important)
   finfo.m_fname[fname.length()] = '\0'; // NOLINT(*-constant-array-index)
   return finfo;
-}
-
-/// It is easy to use std::unique_ptr here for the mem_block
-/// but maybe there is the benefit in proper using of the raw pointers?
-/// It is easy to rewrite if requested!
-[[nodiscard]] rc File::alloc() noexcept
-{
-  static constexpr auto fn{ "File::heap_alloc()" };
-  try {
-    m_block = { new char_type[m_block_size]{} };
-    if (!m_block) {
-      WNDX_LOG(LL::CRIT, "{} {}\n", fn, rc::MEMORY_BLOCK_EMPTY);
-      return rc::MEMORY_BLOCK_EMPTY;
-    }
-    WNDX_LOG(LL::DBUG, "{}\n", fn);
-  } catch (std::bad_alloc const& err) {
-    WNDX_LOG(LL::ERRO, "{} {}\n{}\n", fn, rc::MEMORY_BAD_ALLOC, err.what());
-    File::~File();
-    return rc::MEMORY_BAD_ALLOC;
-  } catch (std::exception const& err) {
-    WNDX_LOG(LL::CRIT, "{} {}\n{}\n", fn, rc::CRIT_EX_UNHANDLED, err.what());
-    File::~File();
-    return rc::CRIT_EX_UNHANDLED;
-  }
-  return rc::SUCCESS;
-}
-
-[[nodiscard]] rc File::write() const noexcept
-{
-  static constexpr auto fn{ "File::write()" };
-  if (!m_block) {
-    WNDX_LOG(LL::CRIT, "{} {}\n", fn, rc::MEMORY_BLOCK_EMPTY);
-    return rc::MEMORY_BLOCK_EMPTY;
-  }
-  // open file for writing
-  std::basic_fstream<char_type> ofs(m_fpath, openmode_w);
-  // FIXME: these checks not valid for the not existing file!
-  //        (file not yet written to the disk)
-  // if (!ofs) {
-  //   WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_CTOR_ERRO);
-  //   return rc::FS_CTOR_ERRO;
-  // }
-  // if (ofs.fail()) {
-  //   WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_OPEN_ERRO);
-  //   return rc::FS_OPEN_ERRO;
-  // }
-  // if (!ofs.good()) {
-  //   WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_IO_UNAVAILABLE);
-  //   return rc::FS_IO_UNAVAILABLE;
-  // }
-  try {
-    // NOTE: for loop is necessary for writing full contents of the binary file!
-    for (size_t i = 0; i < m_block_size; i++) {
-      ofs << m_block[i]; // NOLINT(*-pointer-arithmetic)
-    }
-    WNDX_LOG(LL::STAT, "{} SUCCESS:\n\t{}\n", fn, m_fpath);
-  } catch (std::exception const& err) {
-    WNDX_LOG(LL::CRIT, "{} {}\n{}\n", fn, rc::CRIT_EX_UNHANDLED, err.what());
-    return rc::CRIT_EX_UNHANDLED;
-  }
-  return rc::SUCCESS;
-}
-
-[[nodiscard]] rc File::read() const noexcept
-{
-  static constexpr auto fn{ "File::fcontent()" };
-  rc                    rc{ is_r(m_fpath) }; // return code
-  if (rc != rc::SUCCESS) {
-    return rc;
-  } // open file for reading
-  std::basic_fstream<char_type> ifs(m_fpath, openmode_r);
-  // invariants & validation/safety checks
-  if (!ifs) {
-    WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_CTOR_ERRO);
-    return rc::FS_CTOR_ERRO;
-  }
-  if (ifs.fail()) {
-    WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_OPEN_ERRO);
-    return rc::FS_OPEN_ERRO;
-  }
-  if (!ifs.good()) {
-    WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::FS_IO_UNAVAILABLE);
-    return rc::FS_IO_UNAVAILABLE;
-  }
-  try { // we check on the ifs.fail() => will not be negative!
-    auto const size{ ifs.tellg() };
-    ifs.seekg(0, ifs.beg);
-    ifs.read(m_block, size);
-    WNDX_LOG(LL::INFO, "{} SUCCESS\n", fn);
-  } catch (std::exception const& err) {
-    WNDX_LOG(LL::CRIT, "{} {}\n{}\n", fn, rc::CRIT_EX_UNHANDLED, err.what());
-    return rc::CRIT_EX_UNHANDLED;
-  }
-  return rc::SUCCESS;
-}
-
-[[nodiscard]] rc File::alloc_and_read() noexcept
-{
-  static constexpr auto fn{ "File::read_to_block()" };
-  rc                    rc = alloc();
-  if (rc != rc::SUCCESS) {
-    return rc;
-  }
-  try {
-    rc = read();
-    if (rc != rc::SUCCESS) {
-      WNDX_LOG(LL::ERRO, "{} -> {}\n", fn, rc);
-      return rc;
-    }
-    WNDX_LOG(LL::DBUG, "{} SUCCESS : {}\n", fn, m_fpath);
-  } catch (std::exception const& err) {
-    WNDX_LOG(LL::CRIT, "{} {}\n{}\n", fn, rc::CRIT_EX_UNHANDLED, err.what());
-    File::~File();
-    return rc::CRIT_EX_UNHANDLED;
-  } catch (...) {
-    WNDX_LOG(LL::CRIT, "{} occurred but was caught!\n{}\n",
-             rc::CRIT_EX_ANONYMOUS, "THIS IS VERY BAD!");
-    return rc::CRIT_EX_ANONYMOUS;
-  }
-  return rc::SUCCESS;
-}
-
-[[nodiscard]] rc File::copy_to(fs::path const& dst) noexcept
-{
-  static constexpr auto fn{ "File::copy_to()" };
-  if (m_block == nullptr) {
-    WNDX_LOG(LL::ERRO, "{} {}\n", fn, rc::MEMORY_BLOCK_EMPTY);
-    return rc::MEMORY_BLOCK_EMPTY;
-  }
-  m_fpath = dst; // set new file path prior to the write to the disk.
-  rc rc   = write();
-  if (rc != rc::SUCCESS) {
-    WNDX_LOG(LL::ERRO, "{} -> {}\n", fn, rc);
-    return rc;
-  }
-  WNDX_LOG(LL::DBUG, "{} SUCCESS : {}\n", fn, m_fpath);
-  return rc::SUCCESS;
-}
-
-[[nodiscard]] rc File::move_to(fs::path const& dst) noexcept
-{
-  static constexpr auto fn{ "File::move_to()" };
-  fs::path const        old_fpath{ m_fpath };
-  rc                    rc = copy_to(dst);
-  if (rc != rc::SUCCESS) {
-    WNDX_LOG(LL::ERRO, "{} -> {}\n", fn, rc);
-    return rc;
-  }
-  std::error_code ec{};
-  fs::remove(old_fpath, ec);
-  if (ec) {
-    WNDX_LOG(LL::ERRO, "[FAIL] fs::remove(old_fpath, ec) -> v:{} m:{}\n",
-             ec.value(), ec.message());
-  } else {
-    WNDX_LOG(LL::DBUG, "[ OK ] fs::remove(\"{}\")\n", old_fpath);
-  }
-  WNDX_LOG(LL::DBUG, "{} SUCCESS : {}\n", fn, m_fpath);
-  return rc::SUCCESS;
-}
-
-void File::print() const noexcept
-{
-  if (m_block == nullptr) {
-    return;
-  }
-  // NOTE: for loop is necessary for printing full contents of the binary file!
-  for (size_t i = 0; i < m_block_size; ++i) {
-    fmt::print("{:c}", m_block[i]); // NOLINT(*-pointer-arithmetic)
-  }
 }
 
 } // namespace wndx::mqlqd::file
